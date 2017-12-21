7d5bd9d189a381879cd582c16f028c2dd170bd47
oyvindjohansen1972 <johansen_oyvind@hotmail.com>
2017-12-21 13:48:34 +0100

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonPause = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Image = ((System.Drawing.Image)(resources.GetObject("buttonStart.Image")));
+            this.buttonStart.Location = new System.Drawing.Point(98, 489);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(122, 124);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "25 : 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 329);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Image = ((System.Drawing.Image)(resources.GetObject("buttonStop.Image")));
+            this.buttonStop.Location = new System.Drawing.Point(354, 489);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(122, 124);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Image = ((System.Drawing.Image)(resources.GetObject("buttonPause.Image")));
+            this.buttonPause.Location = new System.Drawing.Point(226, 489);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(122, 124);
+            this.buttonPause.TabIndex = 4;
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(568, 657);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pomodoro 1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonPause;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int second = 0;
+        private int minute = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string doLongTask()
+        {
+            string result = new WebClient().DownloadString("http://msdn.microsoft.com");
+            return "Finished!" + result;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Started";            
+            Task<string> task = new Task<string>(doLongTask);
+            task.Start();
+
+           
+            label1.Text = await task;
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (second > 0)
+            {
+                second--;
+            }
+            else
+            {
+                if (minute > -1)
+                {                                        
+                    if(minute == 0)
+                    {
+                        //finished counting down!
+                        timer1.Enabled = false;
+                        buttonStart.Enabled = true;
+                        SoundPlayer audio = new SoundPlayer(WindowsFormsApplication1.Properties.Resources.Picked);
+                        audio.Play();
+                        this.BringToFront();
+                        this.TopMost = true;
+                    }
+                    else
+                    {
+                        second = 59;
+                        minute--;
+                    }
+                }               
+            }
+            label1.Text = minute + " : " + second;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+            timer1.Enabled = true;
+            second = 59;
+            minute = 24;
+            buttonStart.Enabled = false;           
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+            timer1.Enabled = false;
+            second = 0;
+            minute = 25;
+            label1.Text = minute + " : " + second;
+            buttonStart.Enabled = true;
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {      
+            if (buttonStart.Enabled == false)
+            {
+                timer1.Enabled = !timer1.Enabled;
+            }           
+        }
+    }
+}
